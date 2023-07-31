16a16ac0dd5bcab44bb81e3694eb2e7c6d0011d8
saurabh d <saurabh.cdac79@gmail.com>
2023-07-31 12:44:28 +0530

    initial commit 31/07

--- src/CodeChallenge_3107.java
+++ src/CodeChallenge_3107.java
@@ -1,5 +1,7 @@
 /*
-Given an unsorted integer array containing many duplicate elements, rearrange it such that the same element appears together and the relative order of the first occurrence of each element remains unchanged.
+Given an unsorted integer array containing many duplicate elements,
+rearrange it such that the same element appears together and
+the relative order of the first occurrence of each element remains unchanged.
         For example:
         Input:  { 1, 2, 3, 1, 2, 1 }
         Output: { 1, 1, 1, 2, 2, 3 }
@@ -9,7 +11,41 @@ Given an unsorted integer array containing many duplicate elements, rearrange it
         Output: { 5, 5, 5, 4, 4, 3, 1, 2, 2 }
 */
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CodeChallenge_3107 {
+
+    static void rearrange(int[] array){
+//        int[] sortedArray = new int[array.length];
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>();
+        for(int i=0;i<array.length;i++){
+            if(set.contains(array[i])){
+                continue;
+            }
+            for(int j=0;j<array.length; j++){
+                if( array[i] == array[j]){
+                    list.add(array[i]);
+                    set.add(array[i]);
+                }
+            }
+        }
+        System.out.println(list);
+    }
+    private static void rearrange1(int[] array) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 1, 2, 1 };
+
+        rearrange1(array);
+    }
+
+
 
 
 }
