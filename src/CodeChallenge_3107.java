ff93caed41a02b9750edaa1c6fa75214b1c0098d
saurabh d <saurabh.cdac79@gmail.com>
2023-07-31 13:15:38 +0530

    initial commit 31/07

--- src/CodeChallenge_3107.java
+++ src/CodeChallenge_3107.java
@@ -11,10 +11,7 @@ the relative order of the first occurrence of each element remains unchanged.
         Output: { 5, 5, 5, 4, 4, 3, 1, 2, 2 }
 */
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CodeChallenge_3107 {
 
@@ -36,12 +33,32 @@ public class CodeChallenge_3107 {
         System.out.println(list);
     }
     private static void rearrange1(int[] array) {
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        for(int i=0; i<array.length; i++){
 
+            if(map.containsKey(array[i])){
+//                int n =map.get(array[i]).intValue();
+
+                map.replace(array[i],map.get(array[i]).intValue()+1);
+//                map.replace(array[i],n,++n);
+                System.out.println("Updated value of "+array[i]+" is "+map.get(array[i]));
+            }else {
+                map.put(array[i],1);
+                System.out.println("Put "+array[i]);
+            }
+
+        }
+        System.out.println(map);
+        for(Map.Entry<Integer, Integer> n : map.entrySet()){
+            for(int num=1;num<=n.getValue();num++){
+                System.out.print(n.getKey()+" ");
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 1, 2, 1 };
-
+//        int[] array = { 1, 2, 3, 1, 2, 1 };
+        int[] array = { 5, 4, 5, 5, 3, 1, 2, 2, 4 };
         rearrange1(array);
     }
 
