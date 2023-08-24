5da19608b4132527781a5309b9bef1bf47b3c3fc
saurabh d <saurabh.cdac79@gmail.com>
2023-08-24 16:21:24 +0530

    commit 24/08

--- src/august/CodeChallenge_2408.java
+++ src/august/CodeChallenge_2408.java
@@ -8,6 +8,7 @@ the mobile keypad which has english alphabets associated with each key.
         Output:
         ADG BDG CDG AEG BEG CEG AFG BFG CFG ADH BDH CDH AEH BEH CEH AFH BFH CFH ADI BDI
         CDI AEI BEI CEI AFI BFI CFI
+
 */
 
 import java.util.ArrayList;
@@ -44,11 +45,49 @@ public class CodeChallenge_2408 {
             System.out.println("\nCount "+count);
 
     }
+
+
     public static void main(String[] args) {
-        int keys = 789;
-        findCombinations(keys);
+        int keys = 7287224;
+        findCombinations2(keys);
 
     }
 
+    private static void findCombinations2(int keys) {
 
+        Integer i = keys;
+        String keyString = i.toString();
+
+        int length = keyString.length();
+
+        if(length == 0){
+            System.out.println("Empty");
+        }
+
+        List<String> comboList = new ArrayList<>();
+
+        findCombos1(0,comboList,keyString,new StringBuilder());
+
+        System.out.println(comboList);
+        System.out.println(comboList.size());
+
+
+    }
+
+    private static void findCombos1(int pos, List<String> comboList, String keyString, StringBuilder stringBuilder) {
+        char[][] data = {{},{},{'A','B','C'},{'D','E','F'},{'G','H','I'},{'J','K','L'},{'M','N','O'}
+                ,{'P','O','R','S'},{'T','U','V'},{'W','X','Y','Z'}};
+
+        if(pos == keyString.length()){
+            comboList.add(stringBuilder.toString());
+            return;
+        }
+//        System.out.println("key "+keyString+" charAt "+ keyString.charAt(pos)+" pos "+ pos+" SB "+stringBuilder+" comboList "+comboList);
+
+        char[] letters = data[Character.getNumericValue(keyString.charAt(pos))];
+
+        for(int i=0;i< letters.length;i++){
+            findCombos1(pos+1, comboList, keyString, new StringBuilder(stringBuilder).append(letters[i]));
+        }
+    }
 }
